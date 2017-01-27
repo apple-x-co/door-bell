0326b12d73fd62c552c5763eedff052d0e8e9fe0
sanokouhei <sanokouhei@10.0.1.8>
2017-01-27 10:23:54 +0900

    change

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 var http = require( 'http' ); // HTTPモジュール読み込み
 var socketio = require( 'socket.io' ); // Socket.IOモジュール読み込み
 var fs = require( 'fs' ); // ファイル入出力モジュール読み込み
+var port = '3000';
+if (typeof(process) !== 'undefined' && process.env.PORT) {
+	port = process.env.PORT;
+}
 
 // 3000番ポートでHTTPサーバーを立てる
 var server = http.createServer( function( req, res ) {
     res.writeHead(200, { 'Content-Type' : 'text/html' }); // ヘッダ出力
     res.end( fs.readFileSync('./index.html', 'utf-8') );  // index.htmlの内容を出力
-}).listen(process.env.PORT || 3000);
+}).listen(port);
 
 // サーバーをソケットに紐付ける
 var io = socketio.listen( server );
